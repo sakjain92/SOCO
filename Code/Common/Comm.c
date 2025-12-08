sakjain92/SOCO

commit a18d920521848c6847edbd25ee84cf492343c554

SOCO: Feature: Add logic for inputs/outputs
Added logic to control the outputs (relays) based on voltages/digital
inputs.

Also, improved the calibration for SOCO (Added digital inputs/outputs
self test as part of calibration).

Code/Common/Comm.c
@@ -324,6 +324,18 @@ Ret: None
 
 #ifdef MODEL_RS485
 
+// Sends data over modbus
+// @param buffer: Buffer containing data to be sent over modbus (should be in big endian)
+// @param length; Length of data in bytes to be sent
+// @param func: The current function code to be sent over
+//
+static void ModbusSendData(uint8_t* buffer, uint8_t length, uint8_t func)
+{
+  memcpy(&Mod_TransmitFrame.Data_Array[1], buffer, length);
+  Mod_TransmitFrame.Data_Array[0] = length;
+  SendData_UART((uint8_t)CopySetPara[PARA_DEVICE_ID], func, (length+1));
+}
+
 #ifdef MODBUS_MAP_PROCOM
 void ModBusCommunication(void)
 {
@@ -654,7 +666,58 @@ void ModBusCommunication(void)
                
               }
               break;
-               
+            case 0x2 /*MODBUS_READ_INPUT_STATUS_CODE*/:
+            {
+                Start_Add_High = Readarray_Modbus[2];
+                Start_Add_Low = Readarray_Modbus[3];
+                Start_Add = (Start_Add_Low) + (Start_Add_High<<8);
+                NoOfReg_High = Readarray_Modbus[4];
+                NoOfReg_Low = Readarray_Modbus[5];
+                NoOfReg = (NoOfReg_Low) + (NoOfReg_High<<8);
+                
+                uint32_t endReg = (uint32_t)(Start_Add) + (uint32_t)(NoOfReg) - 1;
+                uint8_t maxRegs = sizeof(g_DigInputs);
+                if (endReg >= maxRegs || NoOfRegs == 0)
+                {
+                    Fun_Received |= 0x80;
+                    Mod_TransmitFrame.Data_Array[0] = 0x02;
+                    SendData_UART(CopySetPara[PARA_DEVICE_ID], Fun_Received,1);
+                    break;
+                }
+
+                // We need to shift certain bits if Start_Add != 0
+                //
+                uint8_t sourceBuffer[64] = {0};
+                uint8_t destBuffer[64] = {0};
+
+                for (int i = 0; i < sizeof(g_DigInputs); i++)
+                {
+                    if (((uint8_t*)(g_DigInputs))[i])
+                    {
+                        int sourceByteIdx = i / 8;
+                        int sourceBitIdx = i - 8 * sourceByteIdx;
+                        sourceBuffer[sourceByteIdx] |= (1 << sourceBitIdx);
+                    }
+                }
+                for (int bitIdx = 0; bitIdx < NoOfReg; bitIdx++)
+                {
+                    int sourceBitIdx = bitIdx + Start_Add;
+                    int sourceByteIdx = sourceBitIdx / 8;
+                    int sourceBitOffset = sourceBitIdx - sourceByteIdx * 8;
+
+                    int destBitIdx = bitIdx;
+                    int destByteIdx = destBitIdx / 8;
+                    int destBitOffset = destBitIdx - destByteIdx * 8;
+
+                    destBuffer[destByteIdx] |=
+                        (((sourceBuffer[sourceByteIdx] >> sourceBitOffset) & 0x1) << destBitOffset);
+                }
+
+                uint8_t numBytesToSend = ROUNDUP_POW2(NoOfReg, 8) / 8;
+                ModbusSendData(destBuffer, numBytesToSend, Fun_Received);
+                break;
+            }
+
        ///// Exception Response for Illegal Function //////    
           default:
             if((Fun_Received != 0x03))
@@ -740,7 +803,6 @@ void ModbusUpdateParameter(uint16_t Address,uint16_t NoOfBytes)
 {
     uint32_t Temp32,TempParaSetting;//TempReset;
     uint8_t error = 0;
-    float CTPrimary,PTPrimary,MaxPower;
     
     for( Temp32=0;Temp32<MAX_PARAM_LIMIT;Temp32++)ModCopySetPara[Temp32]=CopySetPara[Temp32];
     for(uint16_t CopyLength = 0; CopyLength<NoOfBytes/2; CopyLength++)
@@ -750,11 +812,6 @@ void ModbusUpdateParameter(uint16_t Address,uint16_t NoOfBytes)
       ModCopySetPara[CopyLength+Address] = (uint16_t)Temp32;    
     }
     Temp32=0;
-    CTPrimary= (float)ModCopySetPara[PARA_CT_PRIMARY];
-    if(ModCopySetPara[PARA_CT_PRI_UNIT]==1)CTPrimary =CTPrimary*10;
-    PTPrimary= (float)ModCopySetPara[PARA_PT_PRIMARY];
-    if(ModCopySetPara[PARA_PT_PRI_UNIT]==1)PTPrimary =PTPrimary*10;
-    MaxPower=sqrt(3)*CTPrimary*PTPrimary;
     while(Temp32<(MAX_PARAM_LIMIT))
     {
       TempParaSetting=ModCopySetPara[Temp32];
@@ -765,10 +822,6 @@ void ModbusUpdateParameter(uint16_t Address,uint16_t NoOfBytes)
       }
       else if(EditParameters[Temp32].DisableType ==0)error = 1;
       Temp32++;
-    }
-    if(MaxPower>100e6)
-    {
-      error = 1;
     }
     if(error || (!EditParaPassStatus))
     {

commit 9b4070409101abf83517f38321d482827d770fdf

SOCO: Bugfix: Fix compilation errors
Bug introduced in a18d920521848c6847edbd25ee84cf492343c554

Code/Common/Comm.c
@@ -668,16 +668,16 @@ void ModBusCommunication(void)
               break;
             case 0x2 /*MODBUS_READ_INPUT_STATUS_CODE*/:
             {
-                Start_Add_High = Readarray_Modbus[2];
-                Start_Add_Low = Readarray_Modbus[3];
+                Start_Add_High = RecieveArray[2];
+                Start_Add_Low = RecieveArray[3];
                 Start_Add = (Start_Add_Low) + (Start_Add_High<<8);
-                NoOfReg_High = Readarray_Modbus[4];
-                NoOfReg_Low = Readarray_Modbus[5];
-                NoOfReg = (NoOfReg_Low) + (NoOfReg_High<<8);
+                NoOfBytes_High = RecieveArray[4];
+                NoOfBytes_Low = RecieveArray[5];
+                NoOfBytes = (NoOfBytes_Low) + (NoOfBytes_High<<8);
                 
-                uint32_t endReg = (uint32_t)(Start_Add) + (uint32_t)(NoOfReg) - 1;
+                uint32_t endReg = (uint32_t)(Start_Add) + (uint32_t)(NoOfBytes) - 1;
                 uint8_t maxRegs = sizeof(g_DigInputs);
-                if (endReg >= maxRegs || NoOfRegs == 0)
+                if (endReg >= maxRegs || NoOfBytes == 0)
                 {
                     Fun_Received |= 0x80;
                     Mod_TransmitFrame.Data_Array[0] = 0x02;
@@ -690,16 +690,17 @@ void ModBusCommunication(void)
                 uint8_t sourceBuffer[64] = {0};
                 uint8_t destBuffer[64] = {0};
 
+                uint8_t* source = (uint8_t*)(&g_DigInputs);
                 for (int i = 0; i < sizeof(g_DigInputs); i++)
                 {
-                    if (((uint8_t*)(g_DigInputs))[i])
+                    if (source[i])
                     {
                         int sourceByteIdx = i / 8;
                         int sourceBitIdx = i - 8 * sourceByteIdx;
                         sourceBuffer[sourceByteIdx] |= (1 << sourceBitIdx);
                     }
                 }
-                for (int bitIdx = 0; bitIdx < NoOfReg; bitIdx++)
+                for (int bitIdx = 0; bitIdx < NoOfBytes; bitIdx++)
                 {
                     int sourceBitIdx = bitIdx + Start_Add;
                     int sourceByteIdx = sourceBitIdx / 8;
@@ -713,7 +714,7 @@ void ModBusCommunication(void)
                         (((sourceBuffer[sourceByteIdx] >> sourceBitOffset) & 0x1) << destBitOffset);
                 }
 
-                uint8_t numBytesToSend = ROUNDUP_POW2(NoOfReg, 8) / 8;
+                uint8_t numBytesToSend = ROUNDUP_POW2(NoOfBytes, 8) / 8;
                 ModbusSendData(destBuffer, numBytesToSend, Fun_Received);
                 break;
             }

Code/Common/Interrupt.c
@@ -451,60 +451,60 @@ void ReadInputs()
     struct DigInputState
     {
         bool* val;
-        uint8_t time;
+        uint8_t timer;
         bool (*read)(void);
     };
 
     // DEVNOTE: Keep this debouncing ticks to less than 1 sec in total
     //
-    const uint8_t maxDebouncingTicks = 5;
+#define MAX_DEBOUNCING_TICK 5
     static struct DigInputState dinStateArray[] =
     {
         {
             .val = &g_DigInputs.MainsRPhaseContactorOn,
-            .timer = maxDebouncingTicks,
+            .timer = MAX_DEBOUNCING_TICK,
             .read = IsRPhaseGridHealthyContactorOn,
         },
         {
             .val = &g_DigInputs.MainsYPhaseContactorOn,
-            .timer = maxDebouncingTicks,
+            .timer = MAX_DEBOUNCING_TICK,
             .read = IsYPhaseGridHealthyContactorOn,
         },
         {
             .val = &g_DigInputs.MainsBPhaseContactorOn,
-            .timer = maxDebouncingTicks,
+            .timer = MAX_DEBOUNCING_TICK,
             .read = IsBPhaseGridHealthyContactorOn,
         },
         {
             .val = &g_DigInputs.LoadOnSolarContactorOn,
-            .timer = maxDebouncingTicks,
+            .timer = MAX_DEBOUNCING_TICK,
             .read = IsLoadOnSolarContactorOn,
         },
         {
-            .val = &g_DigInputs.LoadOnMainsContactorOn,
-            .timer = maxDebouncingTicks,
-            .read = IsLoadOnMainsContactorOn,
+            .val = &g_DigInputs.LoadOnGridContactorOn,
+            .timer = MAX_DEBOUNCING_TICK,
+            .read = IsLoadOnGridContactorOn,
         },
         {
             .val = &g_DigInputs.SPDFailed,
-            .timer = maxDebouncingTicks,
+            .timer = MAX_DEBOUNCING_TICK,
             .read = IsSPDFailed,
         },
         {
             .val = &g_DigInputs.DGOff,
-            .timer = maxDebouncingTicks,
+            .timer = MAX_DEBOUNCING_TICK,
             .read = IsDGOff,
         },
         {
             .val = &g_DigInputs.DC48Available,
-            .timer = maxDebouncingTicks,
+            .timer = MAX_DEBOUNCING_TICK,
             .read = IsDC48Available,
         },
     };
 
     for (uint8_t i = 0; i < ARRAY_SIZE(dinStateArray); i++)
     {
-        struct DigInputState* dinState = dinStateArray[i];
+        struct DigInputState* dinState = &dinStateArray[i];
 
         if (dinState->read() != *dinState->val)
         {
@@ -515,14 +515,15 @@ void ReadInputs()
             if (!dinState->timer)
             {
                 *dinState->val = !*dinState->val;
-                dinState->timer = maxDebouncingTicks;
+                dinState->timer = MAX_DEBOUNCING_TICK;
             }
         }
         else
         {
-            dinState->timer = maxDebouncingTicks;
+            dinState->timer = MAX_DEBOUNCING_TICK;
         }
     }
+#undef MAX_DEBOUNCING_TICK
 }
 
 uint16_t freqcounter;

Code/Common/Main.c
@@ -10,6 +10,7 @@
 
 
 #include "includes.h"
+#include "extern_includes.h"
 
 extern const struct PH_COEFF_STRUCT CalibrationCoeff;
 
@@ -200,13 +201,13 @@ void ProcessRelays()
     };
 
     static bool initialized = false;
-    const uint16_t maxContactorChangeLatency = 2;
+#define MAX_CONTACTOR_CHANGE_LATENCY    3
 
     // By default, contactor is turned on when the controller is off
     // (Contactor coil tied to NC of the controller's relay).
     // So assume healthy state initially (except Load On Solar contactor)
     //
-    static struct PhaseVolState pvsArray = 
+    static struct PhaseVolState pvsArray[] = 
     {
         // Mains R Phase
         {
@@ -217,7 +218,7 @@ void ProcessRelays()
             SwitchOffContactorRPhaseGridHealthy,
             &g_DigInputs.MainsRPhaseContactorOn,
             true,
-            maxContactorChangeLatency,
+            MAX_CONTACTOR_CHANGE_LATENCY,
             false,
         },
         // Mains Y Phase
@@ -229,7 +230,7 @@ void ProcessRelays()
             SwitchOffContactorYPhaseGridHealthy,
             &g_DigInputs.MainsYPhaseContactorOn,
             true,
-            maxContactorChangeLatency,
+            MAX_CONTACTOR_CHANGE_LATENCY,
             false,
         },
         // Mains B Phase
@@ -241,9 +242,9 @@ void ProcessRelays()
             SwitchOffContactorBPhaseGridHealthy,
             &g_DigInputs.MainsBPhaseContactorOn,
             true,
-            maxContactorChangeLatency,
+            MAX_CONTACTOR_CHANGE_LATENCY,
             false,
-        }
+        },
         // Load On Solar
         //
         {
@@ -254,7 +255,7 @@ void ProcessRelays()
             SwitchOffContactorLoadOnSolar,
             &g_DigInputs.LoadOnSolarContactorOn,
             false,
-            maxContactorChangeLatency,
+            MAX_CONTACTOR_CHANGE_LATENCY,
             false,
         },
         // Load On Grid
@@ -267,7 +268,7 @@ void ProcessRelays()
             SwitchOffContactorLoadOnGrid,
             &g_DigInputs.LoadOnGridContactorOn,
             true,
-            maxContactorChangeLatency,
+            MAX_CONTACTOR_CHANGE_LATENCY,
             false,
         }
     };
@@ -284,7 +285,7 @@ void ProcessRelays()
     //
     for (uint8_t i = 0; i < 3; i++)
     {
-        struct PhaseVolState* pvs = pvsArray[i];
+        struct PhaseVolState* pvs = &pvsArray[i];
 
         if (pvs->isHealthy)
         {
@@ -301,7 +302,7 @@ void ProcessRelays()
                 if (!pvs->TimeLeft)
                 {
                     pvs->isHealthy = false;
-                    pvs->TimeLeft = CopySetPara[PARA_MAINS_RESET_DELAY];
+                    pvs->TimeLeft = CopySetPara[PARA_MAINS_RETURN_DELAY];
                 }
             }
             else
@@ -328,12 +329,12 @@ void ProcessRelays()
             }
             else
             {
-                pvs->TimeLeft = CopySetPara[PARA_MAINS_RESET_DELAY];
+                pvs->TimeLeft = CopySetPara[PARA_MAINS_RETURN_DELAY];
             }
         }
     }
 
-    struct PhaseVolState* solarPvs = pvsArray[3];
+    struct PhaseVolState* solarPvs = &pvsArray[3];
 
     // We can't read solar's voltages when any of the R/Y/B contactor is still
     // on. Note: Feedback makes only sense when 48VDC is available
@@ -349,32 +350,32 @@ void ProcessRelays()
     if (isAnyPhaseContactorOn)
     {
         solarPvs->isHealthy = false;
-        solarPvs->TimeLeft = CopySetPara[PARA_SOLAR_RESET_DELAY];
+        solarPvs->TimeLeft = CopySetPara[PARA_SOLAR_RETURN_DELAY];
     }
     else if (solarPvs->isHealthy)
     {
         // UNDONE: Check solar phases here
         //
         //bool curUnhealthy = 
-        //    (*pvs->vol > CopySetPara[PARA_SOLAR_OVER_VOLT]) ||
-        //    (*pvs->vol < CopySetPara[PARA_SOLAR_UNDER_VOLT]);
+        //    (*solarPvs->vol > CopySetPara[PARA_SOLAR_OVER_VOLT]) ||
+        //    (*solarPvs->vol < CopySetPara[PARA_SOLAR_UNDER_VOLT]);
         bool curUnhealthy = false;
 
         if (curUnhealthy)
         {
-            if (pvs->TimeLeft)
+            if (solarPvs->TimeLeft)
             {
-                pvs->TimeLeft--;
+                solarPvs->TimeLeft--;
             }
             else
             {
-                pvs->isHealthy = false;
-                pvs->TimeLeft = CopySetPara[PARA_SOLAR_RESET_DELAY];
+                solarPvs->isHealthy = false;
+                solarPvs->TimeLeft = CopySetPara[PARA_SOLAR_RETURN_DELAY];
             }
         }
         else
         {
-            pvs->TimeLeft = CopySetPara[PARA_SOLAR_FAIL_DELAY];
+            solarPvs->TimeLeft = CopySetPara[PARA_SOLAR_FAIL_DELAY];
         }
     }
     else
@@ -386,19 +387,19 @@ void ProcessRelays()
 
         if (curHealthy)
         {
-            if (pvs->TimeLeft)
+            if (solarPvs->TimeLeft)
             {
-                pvs->TimeLeft--;
+                solarPvs->TimeLeft--;
             }
             else
             {
-                pvs->isHealthy = true;
-                pvs->TimeLeft = CopySetPara[PARA_SOLAR_FAIL_DELAY];
+                solarPvs->isHealthy = true;
+                solarPvs->TimeLeft = CopySetPara[PARA_SOLAR_FAIL_DELAY];
             }
         }
         else
         {
-            pvs->TimeLeft = CopySetPara[PARA_SOLAR_RETURN_DELAY];
+            solarPvs->TimeLeft = CopySetPara[PARA_SOLAR_RETURN_DELAY];
         }
     }
 
@@ -443,21 +444,21 @@ void ProcessRelays()
     // So hence should put load on solar. For that, need to shut off the
     // phase contactors
     //
-    pvsArray[0].shouldContactorBeOn = pvsArray[0].isHeathly &&
+    pvsArray[0].shouldContactorBeOn = pvsArray[0].isHealthy &&
                                         !pvsArray[3].isContactorStuck;
-    pvsArray[1].shouldContactorBeOn = pvsArray[0].isHeathly &&
-                                        pvsArray[1].isHeathly &&
+    pvsArray[1].shouldContactorBeOn = pvsArray[0].isHealthy &&
+                                        pvsArray[1].isHealthy &&
                                         !pvsArray[3].isContactorStuck;
-    pvsArray[2].shouldContactorBeOn = pvsArray[0].isHeathly &&
-                                        pvsArray[2].isHeathly &&
+    pvsArray[2].shouldContactorBeOn = pvsArray[0].isHealthy &&
+                                        pvsArray[2].isHealthy &&
                                         !pvsArray[3].isContactorStuck;
-    pvsArray[3].shouldContactorBeOn = pvsArray[3].isHeathly &&
-                                        (g_DigInputs.DgOff || !g_DigInputs.DC48Available) &&
+    pvsArray[3].shouldContactorBeOn = pvsArray[3].isHealthy &&
+                                        (g_DigInputs.DGOff || !g_DigInputs.DC48Available) &&
                                         !pvsArray[0].isContactorStuck &&
                                         !pvsArray[1].isContactorStuck &&
                                         !pvsArray[2].isContactorStuck &&
                                         !pvsArray[4].isContactorStuck;
-    pvsArray[4].shouldContactorBeOn = pvsArray[0].isHeathly &&
+    pvsArray[4].shouldContactorBeOn = pvsArray[0].isHealthy &&
                                         !pvsArray[3].isContactorStuck;
 
     for (uint8_t i = 0; i < 5; i++)
@@ -483,16 +484,17 @@ void ProcessRelays()
             }
             if (!pvsArray[i].ContactorChangeTimeLeft)
             {
-                pvsArray[i].ContactorChangeTimeLeft = maxContactorChangeLatency;
+                pvsArray[i].ContactorChangeTimeLeft = MAX_CONTACTOR_CHANGE_LATENCY;
                 pvsArray[i].isContactorStuck = true;
             }
         }
         else
         {
-            pvsArray[i].ContactorChangeTimeLeft = maxContactorChangeLatency;
+            pvsArray[i].ContactorChangeTimeLeft = MAX_CONTACTOR_CHANGE_LATENCY;
             pvsArray[i].isContactorStuck = false;
         }
     }
+#undef MAX_CONTACTOR_CHANGE_LATENCY
 }
 
 /*
@@ -826,17 +828,17 @@ void StartCalibration(void)
    if (FlagDirectCalibration >= CALIBRATE_IN_START &&
         FlagDirectCalibration <= CALIBRATE_IN_8)
    {
-       const uint8_t* inputs[] = 
+       const bool* inputs[] = 
        {
            &g_DigInputs.MainsRPhaseContactorOn,
            &g_DigInputs.MainsYPhaseContactorOn,
            &g_DigInputs.MainsBPhaseContactorOn,
            &g_DigInputs.LoadOnSolarContactorOn,
-           &g_DigInputs.LoadOnMainsContactorOn,
+           &g_DigInputs.LoadOnGridContactorOn,
            &g_DigInputs.SPDFailed,
            &g_DigInputs.DGOff,
            &g_DigInputs.DC48Available
-       }
+       };
        if (FlagDirectCalibration == CALIBRATE_IN_START)
        {
             for (uint8_t i = 0; i < ARRAY_SIZE(inputs); i++)
@@ -859,14 +861,14 @@ void StartCalibration(void)
    if (FlagDirectCalibration >= CALIBRATE_OUT_1 &&
        FlagDirectCalibration <= CALIBRATE_OUT_5)
    {
-       void (*outputs)(void)[] = 
+       void (*outputs[])(void) = 
        {
            SwitchOnContactorRPhaseGridHealthy,
            SwitchOnContactorYPhaseGridHealthy,
            SwitchOnContactorBPhaseGridHealthy,
            SwitchOnContactorLoadOnSolar,
            SwitchOnContactorLoadOnGrid
-       }
+       };
        uint8_t idx = FlagDirectCalibration - CALIBRATE_OUT_1;
        DisplayOutputX(idx + 1);
        outputs[idx]();
@@ -924,7 +926,7 @@ void SetWorkingGainBuffer(void)
     else
     {
       Slope=(CalibrationCoeff.IR_HIGH_GAIN-CalibrationCoeff.IR_MID_GAIN)/(CUR_HIGH_CAL_POINT-CUR_MID_CAL_POINT);
-      Offset=CalibrationCoeff.IR_HIGH_GAIN-*CUR_HIGH_CAL_POINTSlope;
+      Offset=CalibrationCoeff.IR_HIGH_GAIN-CUR_HIGH_CAL_POINT*Slope;
       WorkingCopyGain.IR_GAIN=Offset+Slope*InstantPara.CurrentR;
     }
   }

Code/Common/ModBus.h
@@ -740,13 +740,13 @@ const struct ModBusParameter BlockAll[]=
  { (uint8_t *)&CopySetPara[PARA_MAINS_UNDER_VOLT_RESET] ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_MAINS_OVER_VOLT_RESET]  ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_MAINS_FAIL_DELAY]       ,          DATA_TYPE_16,     1       ,       1      },
- { (uint8_t *)&CopySetPara[PARA_MAINS_RESET_DELAY]      ,          DATA_TYPE_16,     1       ,       1      },
+ { (uint8_t *)&CopySetPara[PARA_MAINS_RETURN_DELAY]      ,         DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_UNDER_VOLT]       ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_OVER_VOLT]        ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_UNDER_VOLT_RESET] ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_OVER_VOLT_RESET]  ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_FAIL_DELAY]       ,          DATA_TYPE_16,     1       ,       1      },
- { (uint8_t *)&CopySetPara[PARA_SOLAR_RESET_DELAY]      ,          DATA_TYPE_16,     1       ,       1      },
+ { (uint8_t *)&CopySetPara[PARA_SOLAR_RETURN_DELAY]      ,         DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_DEVICE_ID]              ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_BAUD_RATE]              ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_PARITY]                 ,          DATA_TYPE_16,     1       ,       1      },
@@ -758,13 +758,13 @@ const struct ModBusParameter BlockAll[]=
  { (uint8_t *)&CopySetPara[PARA_MAINS_UNDER_VOLT_RESET] ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_MAINS_OVER_VOLT_RESET]  ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_MAINS_FAIL_DELAY]       ,          DATA_TYPE_16,     1       ,       1      },
- { (uint8_t *)&CopySetPara[PARA_MAINS_RESET_DELAY]      ,          DATA_TYPE_16,     1       ,       1      },
+ { (uint8_t *)&CopySetPara[PARA_MAINS_RETURN_DELAY]      ,         DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_UNDER_VOLT]       ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_OVER_VOLT]        ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_UNDER_VOLT_RESET] ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_OVER_VOLT_RESET]  ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_SOLAR_FAIL_DELAY]       ,          DATA_TYPE_16,     1       ,       1      },
- { (uint8_t *)&CopySetPara[PARA_SOLAR_RESET_DELAY]      ,          DATA_TYPE_16,     1       ,       1      },
+ { (uint8_t *)&CopySetPara[PARA_SOLAR_RETURN_DELAY]      ,         DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_DEVICE_ID]              ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_BAUD_RATE]              ,          DATA_TYPE_16,     1       ,       1      },
  { (uint8_t *)&CopySetPara[PARA_PARITY]                 ,          DATA_TYPE_16,     1       ,       1      },

Code/Common/Struct.h
@@ -2,6 +2,7 @@
 #define STRUCT_H
 
 #include "stm32f37x.h"
+#include <stdbool.h>
 
 struct STORE 
 {
@@ -406,7 +407,7 @@ struct DigInputs
     bool MainsYPhaseContactorOn;
     bool MainsBPhaseContactorOn;
     bool LoadOnSolarContactorOn;
-    bool LoadOnMainsContactorOn;
+    bool LoadOnGridContactorOn;
     bool SPDFailed;             
     bool DGOff;              
     bool DC48Available;          

Code/Common/extern_includes.h
@@ -1,6 +1,8 @@
 #ifndef EXTERN_INCLUDES_H
 #define EXTERN_INCLUDES_H
 
+#include <string.h>
+
 extern void Delay1Msec12Mhz( uint32_t Delay);
 extern void DisplayParameterUpdate(void); 
 extern void ProtectionReset(void); 
@@ -116,5 +118,17 @@ extern void SwitchOnContactorBPhaseGridHealthy();
 extern void SwitchOffContactorLoadOnSolar();
 extern void SwitchOnContactorLoadOnSolar();
 extern void SwitchOffContactorLoadOnGrid();
-extern void SwitchOnContactorLoadOnGrid(;)
+extern void SwitchOnContactorLoadOnGrid();
+
+
+#define RUNNING_MODE_IMPORT  0
+
+#define TO_BOOL(x)      (!!(x))
+
+#define ARRAY_SIZE(a)           (sizeof(a) / sizeof(a[0]))
+
+// Rounds up X (integer) to an upper integer multiple of 'factor' where 'factor' is power of two
+//
+#define ROUNDUP_POW2(x, factor)  ((((x)+(factor)-1) & ~((factor) - 1)))
+
 #endif

Code/Common/includes.h
@@ -6,7 +6,6 @@
 #include "stm32f37x_flash.h"
 #include <Math.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 
 /* Local functions included */
@@ -131,15 +130,5 @@ uint16_t BufferToDisplay[24];
 uint8_t DisplayParameterBuffer[50],dispStartCnt,DisParalast;
 
 struct DigInputs g_DigInputs;
-
-#define RUNNING_MODE_IMPORT  0
-
-#define TO_BOOL(x)      (!!(x))
-
-#define ARRAY_SIZE(a)           (sizeof(a) / sizeof(a[0]))
-
-// Rounds up X (integer) to an upper integer multiple of 'factor' where 'factor' is power of two
-//
-#define ROUNDUP_POW2(x, factor)  ((((x)+(factor)-1) & ~((factor) - 1)))
 
 #endif

Code/User/EditPara_DIN.c
@@ -359,7 +359,7 @@ void UpdateEditSettings(void)
    Delay1Msec12Mhz(2);
    SetMeterParameters();
 #ifdef MODEL_DATA_SAVE
-   uint8_t i,k=0;
+   uint8_t k=0;
       
    InterruptFlag |=INT_DATA_SAVING_EEPROM;
    __no_operation();

Code/User/FlagDef_DIN.h
@@ -349,11 +349,11 @@ define region CoeffDataLoc = mem:[from 0x0800FF00 to 0x0800FFFF];
 // Input from 48V is NC when it's healthy
 // Input from DG is NO when it's running
 //
-#define INPUT_R_PHASE_GRID_HEALTHY_CONTACTOR_ON    TO_BOOL(!(GPIOE->IDR & PORT_BIT_0)
-#define INPUT_Y_PHASE_GRID_HEALTHY_CONTACTOR_ON    TO_BOOL(!(GPIOE->IDR & PORT_BIT_1)
-#define INPUT_B_PHASE_GRID_HEALTHY_CONTACTOR_ON    TO_BOOL(!(GPIOE->IDR & PORT_BIT_2)
-#define INPUT_LOAD_ON_SOLAR_CONTACTOR_ON           TO_BOOL(!(GPIOE->IDR & PORT_BIT_3)
-#define INPUT_LOAD_ON_GRID_CONTACTOR_ON            TO_BOOL(!(GPIOE->IDR & PORT_BIT_4)
+#define INPUT_R_PHASE_GRID_HEALTHY_CONTACTOR_ON    TO_BOOL(!(GPIOE->IDR & PORT_BIT_0))
+#define INPUT_Y_PHASE_GRID_HEALTHY_CONTACTOR_ON    TO_BOOL(!(GPIOE->IDR & PORT_BIT_1))
+#define INPUT_B_PHASE_GRID_HEALTHY_CONTACTOR_ON    TO_BOOL(!(GPIOE->IDR & PORT_BIT_2))
+#define INPUT_LOAD_ON_SOLAR_CONTACTOR_ON           TO_BOOL(!(GPIOE->IDR & PORT_BIT_3))
+#define INPUT_LOAD_ON_GRID_CONTACTOR_ON            TO_BOOL(!(GPIOE->IDR & PORT_BIT_4))
 #define INPUT_SPD_HEALTHY                          TO_BOOL(GPIOE->IDR & PORT_BIT_5)
 #define INPUT_DG_RUNNING                           TO_BOOL(GPIOE->IDR & PORT_BIT_6)
 #define INPUT_48V_AVAILABLE                        TO_BOOL(!(GPIOF->IDR & PORT_BIT_10))

Code/User/Model_DIN.h
@@ -1,4 +1,4 @@
-#define MODEL_RELEASED
+//#define MODEL_RELEASED
 
 #define MODEL_COMM_PROCOM
 #define MODEL_DATA_SAVE  
@@ -81,6 +81,7 @@
 // 55) For whole current, how do I calibrate at 100A?
 // 56) We should make it possible to see state of outputs/inputs on the controller otherwise will be difficult to debug issues in the field (contactor latching etc)
 // 57) Since we use NC or NO of a relay, do we want to just have one output per relay on board (less chances of mistake when making the panel)
+// 58) For four quarant metering, we need to fix P1/P2 & S1/S2 of CT with CT polarity correction. Possibly have to get it tested on panel side also via a WebUSB based RS485 testing software
 //
 // NOTES:
 // 1) For Apparent power, we are using RMS (IEC 60038) instead of (IEC 61000-4-7) where we just measure first harmonics as we need to match energy with utility meter (and SMPS will have noise)